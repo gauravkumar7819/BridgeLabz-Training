107c9417158ad9c1e2b745d2726c7df7ca9f4642
Gaurav Kumar <gaurav.kumar_cs@gla.ac.in>
2025-12-17 14:53:30 +0530

    Remove Element

--- core-java-practice/leet-code-codebase/removeElement.java
+++ core-java-practice/leet-code-codebase/removeElement.java
@@ -1,7 +1,7 @@
 class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] arr = {3,2,2,3};
+        int[] arr = { 3, 2, 2, 3 };
         int target = 3;
         int newLength = sol.removeElement(arr, target);
         System.out.println("New length: " + newLength);
@@ -10,20 +10,19 @@ class Solution {
             System.out.print(arr[i] + " ");
         }
     }
-public int removeElement(int[] arr, int target) {
-int k=0;
-int p=0;
-for (int i = 0; i < arr.length; i++) {
-        if(arr[i]!=target) {
-                    arr[k]=arr[i];
-                    k++;
-                            }
-                            else{
-                                p++;
-                            }
-                                }
 
+    public int removeElement(int[] arr, int target) {
+        int k = 0;
+        int p = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != target) {
+                arr[k] = arr[i];
+                k++;
+            } else {
+                p++;
+            }
+        }
 
-return arr.length-p;
-}
+        return arr.length - p;
+    }
 }
