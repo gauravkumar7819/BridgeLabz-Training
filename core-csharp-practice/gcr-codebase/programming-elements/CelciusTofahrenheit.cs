965445a894f044c68ab023d3683d24fb7f63d574
Gaurav Kumar <gaurav.kumar_cs@gla.ac.in>
2025-12-18 15:33:56 +0530

    [Gaurav Kumar] Celcius To Fahrenheit

---
+++ core-csharp-practice/gcr-codebase/programming-elements/CelciusTofahrenheit.cs
@@ -0,0 +1,12 @@
+using System;
+class CilciusToFahrenheit
+{
+    static void Main()
+    {
+        double celsius = 25;
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine("Temperature in Fahrenheit is: " + fahrenheit);
+    }
+}
