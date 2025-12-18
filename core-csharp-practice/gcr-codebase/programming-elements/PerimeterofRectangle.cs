72a794e041feb085beadc421503f9a437e0d0c41
Gaurav Kumar <gaurav.kumar_cs@gla.ac.in>
2025-12-18 15:35:32 +0530

    [Gaurav Kumar] Perimeter of Rectangle

---
+++ core-csharp-practice/gcr-codebase/programming-elements/PerimeterofRectangle.cs
@@ -0,0 +1,12 @@
+using System;
+class PerimeterofRectangle{
+static void Main()
+{
+    int l = 20;
+    int b = 10;
+
+    int p = 2 * (l + b);
+
+    Console.WriteLine("Perimeter of Rectangle is: " + p);
+}
+}
