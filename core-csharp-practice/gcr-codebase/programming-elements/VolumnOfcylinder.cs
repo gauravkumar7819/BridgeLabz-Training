e0bde9c6854a864d1c7d24a5d76971a27daf7e1c
Gaurav Kumar <gaurav.kumar_cs@gla.ac.in>
2025-12-18 15:37:00 +0530

    [Gaurav Kumar] Volume Of Cylinder

---
+++ core-csharp-practice/gcr-codebase/programming-elements/VolumnOfcylinder.cs
@@ -0,0 +1,14 @@
+using System;
+
+class VolumeOfCylinder
+{
+    static void Main()
+    {
+        int radius = 10;
+        int height = 15;
+
+        double volume = 3.14 * radius * radius * height;
+
+        Console.WriteLine("Volume of Cylinder is: " + volume);
+    }
+}
