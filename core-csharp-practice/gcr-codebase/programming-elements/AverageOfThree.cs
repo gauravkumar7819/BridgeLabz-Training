940246a14abebabdbaa7b64477c6e707a08ec8ed
Gaurav Kumar <gaurav.kumar_cs@gla.ac.in>
2025-12-18 15:30:57 +0530

    [Gaurav Kumar] Average Of Three

---
+++ core-csharp-practice/gcr-codebase/programming-elements/AverageOfThree.cs
@@ -0,0 +1,14 @@
+using System;
+class AverageOfThree
+{
+    static void Main()
+    {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        double average = (num1 + num2 + num3) / 3.0;
+
+        Console.WriteLine("Average of Three Numbers is: " + average);
+    }
+}
