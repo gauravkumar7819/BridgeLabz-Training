80ab506cc393828602d358f406f13a88d7e56e0e
Gaurav Kumar <gaurav.kumar_cs@gla.ac.in>
2025-12-19 16:34:13 +0530

    [Gaurav Kumar] Add. operators in C#

---
+++ core-csharp-practice/gcr-codebase/programming-elements/Operators.cs
@@ -0,0 +1,10 @@
+using System;
+class Operators{
+static void Main(String []args){
+// Arithmetic Operators
+int a=10;   
+int b=5;
+Console.WriteLine("a + b = " + (a + b)); // Addition
+Console.WriteLine("a - b = " + (a - b)); // Subtraction	
+	
+}}
