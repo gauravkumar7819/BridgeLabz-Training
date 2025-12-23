54a39a087287e51a9ea9fda4243098e5a5767e73
Gaurav Kumar <gaurav.kumar_cs@gla.ac.in>
2025-12-23 10:37:13 +0530

    [Gaurav Kumar] Update Readme file for 22  december

--- core-csharp-practice/gcr-codebase/csharp-control-flows/level1/CountDownFor.cs
+++
@@ -1,9 +0,0 @@
-using System;
-class CountDownFor{
-static void Main(){
-    int inpValue=Convert.ToInt32(Console.ReadLine());
-    for(int i=inpValue;i>=0;i--){
-        Console.WriteLine(i);
-    }
-    
-}}
