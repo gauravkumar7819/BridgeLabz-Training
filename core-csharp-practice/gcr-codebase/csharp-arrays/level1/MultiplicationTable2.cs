5332f181fd9e2af0004eaf7b3cb5956b52d683d5
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2025-12-24 16:43:23 +0530

    [Gaurav Kumar] Refactored. MultiplicationTable2

--- core-csharp-practice/gcr-codebase/csharp-arrays/level1/MultiplicationTable2.cs
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level1/MultiplicationTable2.cs
@@ -10,6 +10,8 @@ class MultiplicationTable2
         Console.WriteLine("Multiplication Table of " + number + " from 6 to 9 is:");
         for (int i = 6; i <= 9; i++)
         {
+
+            
             Console.WriteLine(number + " x " + i + " = " + (number * i));
         }
     }
