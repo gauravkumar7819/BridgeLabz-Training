088a4a5dbbb04bc26268bc77a2d01aa05fe1d202
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2025-12-24 16:32:47 +0530

    [Gaurav Kumar] add. Multiplication table v2

---
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level1/MultiplicationTable2.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MultiplicationTable2
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Multiplication Table of " + number + " from 6 to 9 is:");
+        for (int i = 6; i <= 9; i++)
+        {
+            Console.WriteLine(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
