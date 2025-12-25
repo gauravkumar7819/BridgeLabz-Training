4d0ea07f76c670b69d341568bc903efa77c0a5f4
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2025-12-25 18:48:03 +0530

    [Gaurav Kumar] add. Quotient and remainder

---
+++ core-csharp-practice/gcr-codebase/csharp-methods/level1/QuotientRemainder.cs
@@ -0,0 +1,24 @@
+using System;
+class QuotientRemainder
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number  ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number  ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+ 
+FindQuotientRemainder(a, b);
+        
+
+    }
+    public static void FindQuotientRemainder(int a, int b)
+    {
+        int quotient = a / b;
+        int remainder = a % b;
+
+        Console.WriteLine("The Quotient is " + quotient + " and Remainder is " + remainder + " of two numbers " + a + " and " + b);
+    }
+}
