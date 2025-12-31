38153af94c3fbf6e3e099f75e842a960c2fcd641
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2025-12-31 18:16:23 +0530

    [Gaurav Kumar] .add bank management system

---
+++ oops-csharp-practice/scenario-based/bank-account-management/User.cs
@@ -0,0 +1,15 @@
+﻿class User
+{
+	public int UserId;
+	public string Name;
+	public string Address;
+	public Account Account;
+
+	public User(int userId, string name, string address, string accountNumber, double initialBalance)
+	{
+		UserId = userId;
+		Name = name;
+		Address = address;
+		Account = new Account(accountNumber, initialBalance);
+	}
+}
