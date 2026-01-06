f97078a81c10d35372b23eef6688ff1dbdfb6779
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2026-01-06 16:20:06 +0530

    [Gaurav Kumar].Add code for Call Log program

---
+++ oops-csharp-practice/scenario-based/CallLog-System/Program.cs
@@ -0,0 +1,26 @@
+﻿using CallLog_System;
+
+namespace CallLog_System
+{
+	class Program
+	{
+		static void Main()
+		{
+			CallLogManager manager = new CallLogManager(10);
+
+			CallLog cl = new CallLog("9876543210", "Network issue", DateTime.Now.AddHours(-3));
+			manager.AddCallLog(cl);
+			
+			//manager.AddCallLog("9123456789", "Billing problem", DateTime.Now.AddHours(-2));
+			//manager.AddCallLog("9988776655", "Network slow speed", DateTime.Now.AddHours(-1));
+			//manager.AddCallLog("9000011111", "SIM not working", DateTime.Now);
+
+			manager.SearchByKeyword("network");
+
+			DateTime startTime = DateTime.Now.AddHours(-2);
+			DateTime endTime = DateTime.Now;
+
+			manager.FilterByTime(startTime, endTime);
+		}
+	}
+}
