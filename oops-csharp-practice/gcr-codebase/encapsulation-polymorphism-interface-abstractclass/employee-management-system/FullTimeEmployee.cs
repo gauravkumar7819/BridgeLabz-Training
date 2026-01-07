54f0b470ac40eeec179bdc433fd497fdd4610364
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2026-01-07 22:54:49 +0530

    [Gaurav Kumar] add. Employee management system project

---
+++ oops-csharp-practice/gcr-codebase/encapsulation-polymorphism-interface-abstractclass/employee-management-system/FullTimeEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+internal class FullTimeEmployee: Employee
+{
+    private double FixedSalary;
+
+    public FullTimeEmployee(double FixedSalary, string employeeName): base(Employee.GetId(), employeeName, FixedSalary)
+    {
+        this.FixedSalary = FixedSalary;
+    }
+
+    public override double CalculateSalary()
+    {
+        return FixedSalary;
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/encapsulation-polymorphism-interface-abstractclass/employee-management-system/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee fullTimeEmp = new FullTimeEmployee(50000, "Amit");
+        fullTimeEmp.AssignDepartment("IT");
+
+        fullTimeEmp.DisplayDetails();
+        Console.WriteLine("Calculated Salary: " + fullTimeEmp.CalculateSalary());
+
+        Employee partTimeEmp = new PartTimeEmployee(80, 500, "Neha");
+        partTimeEmp.AssignDepartment("HR");
+
+        partTimeEmp.DisplayDetails();
+        Console.WriteLine("Calculated Salary: " + partTimeEmp.CalculateSalary());
+    }
+}
