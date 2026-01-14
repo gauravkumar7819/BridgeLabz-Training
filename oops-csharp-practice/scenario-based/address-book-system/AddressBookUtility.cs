b0b858d415d9e8a83421cd5d82aa7463a870ce24
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2026-01-14 16:14:26 +0530

    [Gaurav kumar].add encapsulated class of contacts

---
+++ oops-csharp-practice/scenario-based/address-book-system/AddressBookUtility.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace address_book_system
+{
+    class AddressBookUtility : IAddressBook
+    {
+        Contact[] contacts = new Contact[5];
+        static int count = 0;
+
+        // Add Contact
+
+        // Display Contacts
+        public void DisplayAll()
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("No contacts found!");
+                return;
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(contacts[i]);
+            }
+        }
+// /UC-3
+
+}
+}
