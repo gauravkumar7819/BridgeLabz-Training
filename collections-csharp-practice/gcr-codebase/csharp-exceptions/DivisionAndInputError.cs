c383da187e904fd03c5bb97b9ab411fa4ced0197
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2026-01-23 21:59:20 +0530

    [Gaurav Kumar]. add solutions  of execption

---
+++ collections-csharp-practice/gcr-codebase/csharp-exceptions/DivisionAndInputError.cs
@@ -0,0 +1,27 @@
+using System;
+
+class DivisionAndInputError
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int result = a / b;
+            Console.WriteLine("Result: " + result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter numbers only");
+        }
+    }
+}
