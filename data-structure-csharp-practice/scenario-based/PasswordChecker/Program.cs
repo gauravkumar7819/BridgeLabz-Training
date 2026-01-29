4caf3967694bf1ae0cb3cc2d1cb573e9803d57f3
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2026-01-29 11:20:27 +0530

    [Gaurav Kumar].add the scenario based question

---
+++ data-structure-csharp-practice/scenario-based/PasswordChecker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static bool found = false;
+
+    static void Crack(char[] chars, string current, int n, string password)
+    {
+        if (found) return;
+
+        if (current.Length == n)
+        {
+            Console.WriteLine(current);
+
+            if (current.Equals(password))
+            {
+                Console.WriteLine("\nPassword Cracked: " + current);
+                found = true;
+            }
+            return;
+        }
+
+        foreach (char ch in chars)
+        {
+            Crack(chars, current + ch, n, password);
+        }
+    }
+
+    static void Main()
+    {
+        char[] characters = { 'a', 'b' };
+        int length = 3;
+        string password = "bab";
+
+        Crack(characters, "", length, password);
+    }
+}
